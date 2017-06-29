fd76ab379b556751b5b6d83eb4311ae11d6ca9d2
Omar <oa.azab@gmail.com>
2017-06-29 17:00:43 +0200

    parse image + UI polish

--- index.android.js
+++ index.android.js
@@ -20,46 +20,78 @@ export default class infraLayer extends Component {
 
   constructor(props) {
     super(props);
+    // setup initial data
     this.state = {
       isLoading: true,
-      textRespone: "loading data ...",
+      textRespone: "Loading data ...",
       data: []
     }
     console.log(this.state.isLoading)
   }
 
   render() {
+    // show loading indicator till we fetch data from internet
     if(this.state.isLoading){
       return (
-        <View style={styles.container}>
-          <ActivityIndicator />
-          <Text>{this.state.textRespone}</Text>
+        <View style={{flex: 1}}>
+          {this.renderAppBar()}
+          <View style={styles.container}>
+            <ActivityIndicator />
+            <Text>{this.state.textRespone}</Text>
+          </View>
         </View>
       );
     }
       return (
+        <View style={{flex: 1}}>
+          {this.renderAppBar()}
           <FlatList
             data={this.state.data}
             renderItem={this.renderItemCard}
             keyExtractor={item => item.id}
+            ItemSeparatorComponent={this.renderSeparator}
             />
+        </View>
       );
   }
 
+  // render App bar
+  renderAppBar = () => {
+    return (
+      <View style={{height: 50, backgroundColor: '#757575', alignItems: 'center', flexDirection: 'row'}}>
+        <Text style={{color: '#fff', fontSize: 18, paddingLeft: 10}}>infraLayer</Text>
+      </View>
+    );
+  }
+
+  // redner seperator
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          backgroundColor: "#CED0CE",
+        }}
+      />
+    );
+  }
+
+  // render item card for the flatlist
   renderItemCard = ({item}) => {
     return(
       <View style={styles.card}>
         <View style={styles.card_left_container}>
-          <Text>{item.date}</Text>
-          <Text style={styles.cover_title}
+          <Text style={styles.card_title}
                 numberOfLines={2}>{item.title}</Text>
           <Text numberOfLines={3}>{item.description}</Text>
+          <Text style={styles.card_date}>{item.date}</Text>
+
         </View>
         <View style={styles.card_right_container}>
           <Image
           resizeMode='cover'
           style={{flex: 1}}
-          source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+          source={{uri: item.img}}
           />
         </View>
       </View>
@@ -67,6 +99,7 @@ export default class infraLayer extends Component {
   }
 
   componentDidMount() {
+    // fetch data from url and parse it
     return fetch('http://www.bbc.com/arabic/worldnews/index.xml')
       .then((response) => response.text())
       .then((response) => {
@@ -94,12 +127,18 @@ export default class infraLayer extends Component {
     console.log('number of entries' + entries.length)
     for (var i=0; i < entries.length; i++) {
       console.log('title'+entries[i].getElementsByTagName("title")[0].textContent);
+      // default image link
+      var imagelink = 'http://cumbrianrun.co.uk/wp-content/uploads/2014/02/default-placeholder.png';
+      // if this entry has an image
+      if(entries[i].getElementsByTagName("link")[0].getElementsByTagName("media:content")[0].hasChildNodes()){
+        var imagelink = entries[i].getElementsByTagName("link")[0].getElementsByTagName("media:content")[0].getElementsByTagName("media:thumbnail")[0].getAttribute('url');
+      }
       objs.push({
       id: i,
       title: entries[i].getElementsByTagName("title")[0].textContent,
       description: entries[i].getElementsByTagName("summary")[0].textContent,
       date: entries[i].getElementsByTagName("published")[0].textContent.substring(0, 10),
-      img: entries[i].getElementsByTagName("summary")[0].textContent,
+      img: imagelink,
       })
     }
     return objs;
@@ -120,15 +159,21 @@ const styles = StyleSheet.create({
     padding: 5,
   },
   card_left_container: {
-    flex: 3,
+    flex: 4,
     padding: 10,
+    justifyContent: 'space-between',
   },
   card_right_container: {
-    flex: 1,
+    flex: 2,
   },
-  cover_title: {
+  card_title: {
     fontSize: 18,
     fontWeight: 'bold',
+    marginBottom: 5
+  },
+  card_date: {
+    fontSize: 10,
+    marginTop: 5
   }
 });
 
